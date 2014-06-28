2b8de294210b7b5f56b23928abc534917415c846
Tim <timothyjmcclung@gmail.com>
2014-06-28 17:24:13 -0400

    added userpage, displays username

--- BackJackRacer/spec/features/user_spec.rb
+++ BackJackRacer/spec/features/user_spec.rb
@@ -11,4 +11,12 @@ describe 'Users' do
       expect(current_path).to eq(new_round_path)
     end
   end
+
+  describe 'profile' do
+  	let!(:user) { create :user }
+  	it 'displays a username' do
+  		visit user_path(id: user.id)
+  		expect(page).to have_content(user.username)
+  	end
+  end
 end
