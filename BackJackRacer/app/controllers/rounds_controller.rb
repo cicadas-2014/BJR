de8b06ff3176877fc396f3c29ef2da8ec8778e73
Lasse Sviland <lassesviland@hotmail.com>
2014-06-28 16:17:32 -0400

    removed name form new user

--- BackJackRacer/app/controllers/rounds_controller.rb
+++ BackJackRacer/app/controllers/rounds_controller.rb
@@ -2,13 +2,10 @@ class RoundsController < ApplicationController
 	include ApplicationHelper
 
 	def new
-		p current_user
-    p session[:user_id]
 
 	end
 
 	def create
-		p current_user
 		round = Round.create(user:current_user)
 		redirect_to round_path(round)
 	end
